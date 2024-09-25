c00a27862a73793ab0a5414e2b84687dc870d8ef
phamvanquochuy <102366023+phamvanquochuy@users.noreply.github.com>
2024-09-25 09:34:42 +0700

    response DTOs

---
+++ backend/src/main/java/com/pvanquochuy/booking_hotel/response/RoomResponse.java
@@ -0,0 +1,38 @@
+package com.pvanquochuy.booking_hotel.response;
+
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.apache.tomcat.util.codec.binary.Base64;
+
+import java.math.BigDecimal;
+import java.sql.Blob;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+public class RoomResponse {
+    private Long id;
+    private String roomType;
+    private BigDecimal roomPrice;
+    private boolean isBooked;
+    private String photo;
+    private List<BookingResponse> bookings;
+
+    public RoomResponse(Long id, String roomType, BigDecimal roomPrice) {
+        this.id = id;
+        this.roomType = roomType;
+        this.roomPrice = roomPrice;
+    }
+
+    public RoomResponse(Long id, String roomType, BigDecimal roomPrice, boolean isBooked,
+                        byte[] photoByte, List<BookingResponse> bookings) {
+        this.id = id;
+        this.roomType = roomType;
+        this.roomPrice = roomPrice;
+        this.isBooked = isBooked;
+        this.photo = photoByte != null ? Base64.encodeBase64String(photoByte) : null;
+        this.bookings = bookings;
+    }
+
+}
