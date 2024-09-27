5158d334d91ef8bf9cdadab065a891bc8d6ed96b
phamvanquochuy <102366023+phamvanquochuy@users.noreply.github.com>
2024-09-27 21:13:35 +0700

    fix dropdown navbar

--- fontend/src/App.jsx
+++ fontend/src/App.jsx
@@ -6,12 +6,15 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/home/Home.jsx";
 import EditRoom from "./components/room/EditRoom.jsx";
 import AddRoom from "./components/room/AddRoom.jsx";
+import NavBar from "./components/layout/NavBar.jsx";
+import Footer from "./components/layout/Footer.jsx";
 
 function App() {
   return (
     <>
       <main>
         <Router>
+          <NavBar />
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/edit-room/:roomId" element={<EditRoom />} />
@@ -19,6 +22,7 @@ function App() {
             <Route path="/add-room" element={<AddRoom />} />
           </Routes>
         </Router>
+        <Footer />
       </main>
     </>
   );
---
+++ fontend/src/components/layout/Footer.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+
+const Footer = () => {
+  let today = new Date();
+  console.log(today.getFullYear());
+
+  return (
+    <footer className="by-dark text-light py-3 footer mt-lg-5">
+      <Container>
+        <Row>
+          <Col xs={12} md={12} className="text-center">
+            <p>&copy; {today.getFullYear()} October Hotel</p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
