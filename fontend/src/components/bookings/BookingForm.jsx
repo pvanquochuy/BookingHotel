a7cc8a3a0519e7f0e73650db76f5f99c420bc166
phamvanquochuy <102366023+phamvanquochuy@users.noreply.github.com>
2024-10-01 11:28:27 +0700

    font end booking form state

---
+++ fontend/src/components/bookings/BookingForm.jsx
@@ -0,0 +1,104 @@
+import React, { useEffect, useState } from "react";
+import { bookRoom, getRoomById } from "../utils/ApiFunctions";
+import { useNavigate, useParams } from "react-router-dom";
+import moment from "moment";
+
+const BookingForm = () => {
+  const [isValidated, setIsValidated] = useState(false);
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [roomPrice, setRoomPrice] = useState(0);
+  const [booking, setBooking] = useState({
+    guestName: "",
+    guestEmail: "",
+    checkInDate: "",
+    checkOutDate: "",
+    numberOfAdults: "",
+    numberOfChildren: "",
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setBooking({ ...booking, [name]: value });
+    setErrorMessage("");
+  };
+
+  const [roomInfo, setRoomInfo] = useState({
+    photo: "",
+    roomType: "",
+    roomPrice: "",
+  });
+
+  const { roomId } = useParams();
+  const navigate = useNavigate();
+
+  const getRoomPriceById = async (roomId) => {
+    try {
+      const response = await getRoomById(roomId);
+      setRoomPrice(response.roomPrice);
+    } catch (error) {
+      throw new Error(error);
+    }
+  };
+
+  useEffect(() => {
+    getRoomPriceById(roomId);
+  }, [roomId]);
+
+  const calculatePayment = () => {
+    const checkInDate = monment(booking.checkInDate);
+    const checkOutDate = moment(booking.checkOutDate);
+    const diffInDays = checkOutDate.diff(checkInDate);
+    const price = roomPrice ? roomPrice : 0;
+    return diffInDays * price;
+  };
+
+  const isGuestValid = () => {
+    const adultCount = parseInt(booking.numberOfAdults);
+    const childrenCount = parseInt(booking.numberOfChildren);
+    const totalCount = adultCount + childrenCount;
+    return totalCount >= 1 && adultCount >= 1;
+  };
+
+  const isCheckOutDateValid = () => {
+    if (
+      !moment(booking.checkOutDate).isSameOrAfter(moment(booking.checkInDate))
+    ) {
+      setErrorMessage("Check-out date must come before check-in date");
+      return false;
+    } else {
+      setErrorMessage("");
+      return true;
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.currentTarget;
+    if (
+      form.checkValidity() === false ||
+      !isCheckOutDateValid() ||
+      !isCheckOutDateValid()
+    ) {
+      e.stopPropagation();
+    } else {
+      setIsSubmitted(true);
+    }
+    setIsValidated(true);
+  };
+
+  const handleBooking = async () => {
+    try {
+      const confirmationCode = await bookRoom(roomId, booking);
+      navigate("/", { state: { message: { confirmationCode } } });
+      setIsSubmitted(true);
+    } catch (error) {
+      setErrorMessage(error.message);
+      navigate("/", { state: { error: { error: errorMessage } } });
+    }
+  };
+
+  return <div></div>;
+};
+
+export default BookingForm;
